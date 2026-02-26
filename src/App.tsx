1ba449b9ab740ec3fa3bb29f6c75b6f4346f1784
espanta2227 <espanta2227@gmail.com>
2026-02-25 22:57:09 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import { Header, BottomNav, ToastContainer } from './components/Layout';
+import HomePage from './pages/HomePage';
+import ExplorePage from './pages/ExplorePage';
+import DealDetailPage from './pages/DealDetailPage';
+import MyCouponsPage from './pages/MyCouponsPage';
+import FavoritesPage from './pages/FavoritesPage';
+import { LoginPage, RegisterPage } from './pages/AuthPages';
+import { BusinessDashboard, CreatePromotionPage, ScanQRPage } from './pages/BusinessPages';
+import ProfilePage from './pages/ProfilePage';
+import NotificationsPage from './pages/NotificationsPage';
+import MapPage from './pages/MapPage';
+import AdminDashboard from './pages/AdminPages';
+import DeployGuidePage from './pages/DeployGuidePage';
+import InstallPrompt from './components/InstallPrompt';
+
+export default function App() {
+  return (
+    <AppProvider>
+      <HashRouter>
+        <div className="min-h-screen bg-gray-50 max-w-lg mx-auto relative shadow-2xl">
+          <Header />
+          <ToastContainer />
+          <main className="pb-2">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/explore" element={<ExplorePage />} />
+              <Route path="/map" element={<MapPage />} />
+              <Route path="/deal/:id" element={<DealDetailPage />} />
+              <Route path="/my-coupons" element={<MyCouponsPage />} />
+              <Route path="/favorites" element={<FavoritesPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/business" element={<BusinessDashboard />} />
+              <Route path="/business/create" element={<CreatePromotionPage />} />
+              <Route path="/business/edit/:id" element={<CreatePromotionPage />} />
+              <Route path="/business/scan" element={<ScanQRPage />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/deploy-guide" element={<DeployGuidePage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/notifications" element={<NotificationsPage />} />
+            </Routes>
+          </main>
+          <BottomNav />
+          <InstallPrompt />
+        </div>
+      </HashRouter>
+    </AppProvider>
+  );
+}
